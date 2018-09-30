6f80c7e1e32b501d3dcee90bb9d98881ce1c031f
fromrd <rirurarura@gmail.com>
2018-09-30 02:33:52 +0000

    レビューの入力チェックを実装。インデントを修正。

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,12 @@
 class ReviewsController < ApplicationController
   def new
     @review = Review.new
-    # @plan_id = params[:plan_id] 
-    # @review.plan_id = params[:format]
   end
 
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
-    # @review.plan_id = @plan.id
+    
     if @review.save
       # ContactMailer.contact_mail(@plan).deliver
       redirect_to plans_path, notice: 'レビューの投稿が完了しました！'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,28 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
+  def new
+    @user = User.new
+  end
+    
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to plans_path(@user.id)
+    else
+      render 'new'
     end
+  end
     
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            redirect_to plans_path(@user.id)
-        else
-            render 'new'
-        end
+  def show
+    @user = User.find(params[:id])
+    @favorites_plans = @user.favorites
+    @reviews_plans = @user.reviews
+  end
+    
+  private
+    
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :country, :image, :image_cache)
     end
-    
-    def show
-        @user = User.find(params[:id])
-        @favorites_plans = @user.favorites
-        @reviews_plans = @user.reviews
-    end
-    
-    private
-    
-        def user_params
-            params.require(:user).permit(:name, :email, :password, :password_confirmation, :country, :image, :image_cache)
-        end
+
 end
